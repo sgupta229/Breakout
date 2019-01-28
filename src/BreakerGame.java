32faf855aa7509104edb9a96f1e582c04475568f
Will Francis <wbf7@duke.edu>
2019-01-28 18:46:08 -0500

    Adding classes

--- src/BreakerGame.java
+++ src/BreakerGame.java
@@ -1,3 +1,22 @@
+
+import javafx.animation.Timeline;
+import javafx.scene.*;
+import javafx.scene.paint.Paint;
+
 public class BreakerGame {
-//here we are
+    private int framesPerSecond;
+    private Timeline gameLoop;
+    private Scene gameSurface;
+    private Group sceneNodes;
+    private SpriteManager spriteManager;
+    private String windowTitle;
+
+
+    public void startGame(){
+
+    }
+
+    private Scene setupGame (int width, int height, Paint background) {
+
+    }
 }
