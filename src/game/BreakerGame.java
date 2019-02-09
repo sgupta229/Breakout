171df92745210fecf5c8eb695b6042fe164c9802
Will Francis <wbf7@duke.edu>
2019-02-09 13:04:18 -0500

    Fixed mouseHandle

--- src/game/BreakerGame.java
+++ src/game/BreakerGame.java
@@ -190,15 +190,13 @@ public class BreakerGame extends Application {
         if (mouseX < 0) {
             mouseX = 0;
         }
-        else if(myBall.getSpeed() == 0) {
+        if(myBall.getSpeed() == 0) {
             myPaddle.setPosition(WIDTH / 2 - myPaddle.getWidth() / 2, HEIGHT - 25 - myPaddle.getHeight() / 2);
         }
         else if (mouseX + myPaddle.getWidth() >= WIDTH) {
             mouseX = WIDTH - myPaddle.getWidth();
         }
-        else {
-            myPaddle.setX(mouseX - myPaddle.getWidth() / 2);
-        }
+            myPaddle.setX(mouseX);
 
     }
 
